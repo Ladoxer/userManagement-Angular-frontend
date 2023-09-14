649c337b389d971ad8477027526e2665d0bc309d
Ladoxer <ladderdoxer@gmail.com>
2023-09-14 13:34:07 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { AppService } from './components/state/app.service';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { postReducer, profileReducer } from './components/state/app.reducer';
+import { appEffects } from './components/state/app.effects';
+import { AdminLoginComponent } from './components/admin-login/admin-login.component';
+import { AdminNavComponent } from './components/admin-nav/admin-nav.component';
+import { AdminCreateuserComponent } from './components/admin-createuser/admin-createuser.component';
+import { AdminUserlistComponent } from './components/admin-userlist/admin-userlist.component';
+import { AdminUsereditComponent } from './components/admin-useredit/admin-useredit.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AdminRoutingModule } from './components/admin-login/admin.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    AdminLoginComponent,
+    AdminNavComponent,
+    AdminCreateuserComponent,
+    AdminUserlistComponent,
+    AdminUsereditComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot({userdetails:profileReducer,allusers:postReducer}),
+    EffectsModule.forRoot([appEffects])
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
